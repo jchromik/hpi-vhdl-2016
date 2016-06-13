9bad780032c2a7fa19a9e50ba8a550ce2e382ba2
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-06-13 13:59:53 +0200

    add auto created files

---
+++ pue1/Hasardsimulation/netgen/synthesis/mux2to1_synthesis.vhd
@@ -0,0 +1,34 @@
+--------------------------------------------------------------------------------
+-- Copyright (c) 1995-2013 Xilinx, Inc.  All rights reserved.
+--------------------------------------------------------------------------------
+--   ____  ____
+--  /   /\/   /
+-- /___/  \  /    Vendor: Xilinx
+-- \   \   \/     Version: P.20131013
+--  \   \         Application: netgen
+--  /   /         Filename: .vhd
+-- /___/   /\     Timestamp: Wed May 25 16:53:52 2016
+-- \   \  /  \ 
+--  \___\/\___\
+--             
+-- Command	: -intstyle ise -ar Structure -tm mux2to1 -w -dir netgen/synthesis -ofmt vhdl -sim mux2to1.ngc mux2to1_synthesis.vhd 
+-- Device	: xc7a100t-1-csg324
+-- Input file	: mux2to1.ngc
+-- Output file	: /home/scandic/Documents/hpi-vhdl-2016/pue1/Hasardsimulation/netgen/synthesis/mux2to1_synthesis.vhd
+-- # of Entities	: 1
+-- Design Name	: mux2to1
+-- Xilinx	: /opt/Xilinx/14.7/ISE_DS/ISE/
+--             
+-- Purpose:    
+--     This VHDL netlist is a verification model and uses simulation 
+--     primitives which may not represent the true implementation of the 
+--     device, however the netlist is functionally correct and should not 
+--     be modified. This file cannot be synthesized and should only be used 
+--     with supported simulation tools.
+--             
+-- Reference:  
+--     Command Line Tools User Guide, Chapter 23
+--     Synthesis and Simulation Design Guide, Chapter 6
+--             
+--------------------------------------------------------------------------------
+
