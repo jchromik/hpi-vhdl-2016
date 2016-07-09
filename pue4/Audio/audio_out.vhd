6748241ac265072d234e3b29b2110ab49204cb3c
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-07-09 21:09:42 +0200

    audio clock divider and key scancode to sound map

--- pue4/Audio/audio_out.vhd
+++ pue4/Audio/audio_out.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -38,9 +39,43 @@ entity audio_out is port (
 end audio_out;
 
 architecture Behavioral of audio_out is
+	component audio_scancode_to_divisor is port (
+		scancode			: in  STD_LOGIC_VECTOR(7 downto 0);
+		clock_divisor	: out INTEGER);
+	end component;
 
+	type audio_clock_states is (HIGH, LOW);
+	
+	signal clock_divisor : INTEGER := 378788; -- for 264 Hz (c')
+	signal clock_counter_max : INTEGER := 0;
+	signal clock_counter : INTEGER := 0;
+	signal audio_clock_state : audio_clock_states := LOW;
 begin
 
+	audio_scancode_to_divisor0 : audio_scancode_to_divisor port map (scancode, clock_divisor);
+
+	clock_divider : process(clock_counter_max, clk)
+	begin
+		if clk'event and clk = '1' then
+			if clock_counter >= clock_counter_max then
+				clock_counter <= 0;
+				if audio_clock_state <= LOW then
+					audio_clock_state <= HIGH;
+				else
+					audio_clock_state <= LOW;
+				end if;
+			else
+				clock_counter <= clock_counter + 1;
+			end if;
+		end if;
+	end process clock_divider;
+	
+	divisor_to_max_counter : process(clock_divisor)
+	begin
+		-- 100 MHz divided by clock_divisor
+		-- result divided again by 2 to achive alternating high and low with correct frequency
+		clock_counter_max <= 100000000 / (clock_divisor * 2);
+	end process divisor_to_max_counter;
 
 end Behavioral;
 
---
+++ pue4/Audio/audio_scancode_to_divisor.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:51:13 07/09/2016 
+-- Design Name: 
+-- Module Name:    audio_scancode_to_divisor - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity audio_scancode_to_divisor is port (
+	scancode 		: in  STD_LOGIC_VECTOR(7 downto 0);
+	clock_divisor	: out INTEGER);
+end audio_scancode_to_divisor;
+
+architecture Behavioral of audio_scancode_to_divisor is
+
+begin
+
+process(scancode)
+	begin
+		case scancode is
+			when "00100011" => clock_divisor <= 336700; -- 297 Hz (d')
+			when "00100100" => clock_divisor <= 303030; -- 330 Hz (e')
+			when "00101011" => clock_divisor <= 284091; -- 352 Hz (f')
+			when "00110100" => clock_divisor <= 252525; -- 396 Hz (g')
+			when "00011100" => clock_divisor <= 227273; -- 440 Hz (a')
+			when "00110011" => clock_divisor <= 202020; -- 495 Hz (h')
+			when others => clock_divisor <= 378788; -- 264 Hz (c');
+		end case;
+	end process;
+
+end Behavioral;
+
