60c23a113e045f4a0fe8de3600c996dee9d360ee
Tobias Zagorni <tobias.zagorni@student.hpi.de>
2016-07-17 00:56:02 +0200

    Abgabe

--- pue4/video/project_main.vhd
+++ pue4/video/project_main.vhd
@@ -1,38 +1,9 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:36:24 07/11/2016 
--- Design Name: 
--- Module Name:    project_main - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity project_main is
-    Port ( red: out  STD_LOGIC_VECTOR(3 downto 0);
-           green: out  STD_LOGIC_VECTOR(3 downto 0);
-           blue: out  STD_LOGIC_VECTOR(3 downto 0);
+    Port ( video: out  STD_LOGIC_VECTOR(11 downto 0);
            hsync: out  STD_LOGIC;
            vsync: out  STD_LOGIC;
 		   segments: out STD_LOGIC_VECTOR(14 downto 0);
@@ -42,19 +13,21 @@ entity project_main is
 		   kbdata: in STD_LOGIC;
 		   reset: in STD_LOGIC;
 		   
+		   program_select: in STD_LOGIC;
+		   
 		   dbg: out STD_LOGIC);
 end project_main;
 
 architecture Behavioral of project_main is
-	component video_out is port (
-		red: out  STD_LOGIC_VECTOR(3 downto 0);
-		green: out  STD_LOGIC_VECTOR(3 downto 0);
-		blue: out  STD_LOGIC_VECTOR(3 downto 0);
-		hsync: out  STD_LOGIC;
-		vsync: out  STD_LOGIC;
-		
+	component video_timing is port(
 		clk: in STD_LOGIC;
-		color: in integer range 0 to 3);
+
+		x: out integer range 0 to 1440-1;
+		y: out integer range 0 to 900-1;
+		hsync: out  std_logic;
+		vsync: out  std_logic;
+		video_enable: out std_logic;
+		logic_enable: out std_logic);
 	end component;
 	
 	component keyboard_in is port (
@@ -75,55 +48,101 @@ architecture Behavioral of project_main is
 		anyKeyDown	: out	STD_LOGIC);
 	end component;
 	
+	component twister_logic is port(
+		clk: in std_logic;
+		key_down: in std_logic;	
+		enable: in std_logic;
+		
+		color: buffer integer range 0 to 3);
+	end component;
+
+	component twister_output is port(
+		clk: in std_logic;		
+		color: in integer range 0 to 3;
+		
+		video: out std_logic_vector(11 downto 0));
+	end component;
+
+	component flappy_logic is port (
+		clk: in  std_logic;
+		enable: in  std_logic;
+		key_down: in std_logic;
+		random: in std_logic_vector(3 downto 0);
+		
+		bird_y: buffer integer range 0 to 900-1 := 300;
+		obstacles: buffer std_logic_vector(15 downto 0);
+		offset: buffer integer range 0 to 358;
+		game_over: buffer std_logic := '1');
+	end component;
+
+	component flappy_output is port(
+		clk : in std_logic;
+		x : in integer range 0 to 1440-1;
+		y : in integer range 0 to 900-1;
+		bird_y : in integer range 0 to 900-1;
+		obstacles: in std_logic_vector(15 downto 0);
+		offset: in integer range 0 to 358;
+		game_over: in std_logic;
+		
+		video : out std_logic_vector (11 downto 0));
+	end component;
+
+	component random_generator is port (
+		clk: in  std_logic;
+		key_down: in std_logic;
+		random: buffer std_logic_vector(3 downto 0));
+	end component;
+
+	
 	signal ready : STD_LOGIC;
 	signal scancode: STD_LOGIC_VECTOR(7 downto 0);
-	signal color: integer range 0 to 3 := 0;
-	
-	signal color_counter: integer range 0 to 50000000 := 0;
-	signal color_counter_max: integer range 0 to 50000000 := 50000000;
 	
 	signal key_down: std_logic;
-
+	
+	signal video_enable: std_logic;
+	signal logic_enable: std_logic;
+	signal twister_color: integer range 0 to 3;
+	signal twister_video: std_logic_vector(11 downto 0);
+	
+	signal flappy_bird_y: integer range 0 to 900-1;
+	signal flappy_obstacles: std_logic_vector(15 downto 0);
+	signal flappy_offset: integer range 0 to 358;
+	signal flappy_game_over: std_logic;
+	signal flappy_video: std_logic_vector(11 downto 0);
+	signal random: std_logic_vector(3 downto 0);
+	
+	signal x: integer range 0 to 1440-1;
+	signal y: integer range 0 to 900-1;
 begin
 	keyboard_in0: keyboard_in port map (clk, kbclk, kbdata, reset, ready, scancode);
-	segment_out0: segment_out port map (clk, ready, reset, scancode, segments,key_down);
+	segment_out0: segment_out port map (clk, ready, reset, scancode, segments, key_down);
+	
+	vt: video_timing port map (clk, x, y, hsync, vsync, video_enable, logic_enable);
+	
+	tl: twister_logic port map (clk, key_down, logic_enable, twister_color);
+	tp: twister_output port map (clk, twister_color, twister_video);
+	
+	fl: flappy_logic port map (clk, logic_enable, key_down, random, flappy_bird_y, flappy_obstacles, flappy_offset, flappy_game_over);
+	fp: flappy_output port map (clk, x, y, flappy_bird_y, flappy_obstacles, flappy_offset, flappy_game_over, flappy_video);
+	
+	rg: random_generator port map (clk, key_down, random);
 	
 	process(clk)
 	begin
 		if clk'event and clk = '1' then
-			if color_counter = color_counter_max then
-				color_counter <= 0;
-				if key_down = '1' then
-					if color_counter_max > 5000000 +10000000 then
-						color_counter_max <= color_counter_max -10000000;
-					else
-						color_counter_max <= 5000000;
-					end if;
+			if video_enable = '1' then
+				if program_select = '1' then
+					video <= twister_video;
 				else
-					if color_counter_max < 50000000 -2000000 then
-						color_counter_max <= color_counter_max +2000000;
-					else
-						color_counter_max <= 50000000;
-					end if;
-				end if;
-				
-				if color_counter_max < 50000000 then
-					if color = 3 then
-						color <= 0;
-					else
-						color <= color + 1;
-					end if;
+					video <= flappy_video;
 				end if;
 			else
-				color_counter <= color_counter + 1;
+				video <= "000000000000";
 			end if;
 		end if;
-		
-		dbg <= key_down;
 	end process;
 	
-	video_out0: video_out port map (red, green, blue, hsync, vsync, clk, color);
+	dbg <= key_down;
 	
-
 end Behavioral;
 
